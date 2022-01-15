a5c6277f7d569c7a582a1d33a760a065d9b5b6f0
Kazuki Shimamoto <jakky0529@gmail.com>
2022-01-15 16:05:46 +0900

    クイズ情報の取得（ダミー）とボタンクリック時に正誤（ダミー）を出す

--- pages/quiz/index.tsx
+++ pages/quiz/index.tsx
@@ -1,58 +1,98 @@
 import { Container, Flex, Box, Button, Text, Center } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
 import Link from 'next/link'
 
+const initialState = {
+  sentence: '',
+  menu: '',
+  answer: null,
+  answered: null,
+}
+
+const getQuiz = (path) => {
+  return {
+    sentence: 'お腹周りの脂肪を落とすのに最適な運動は腹筋である',
+    menu: '腹筋',
+    answer: false,
+    answered: false,
+  }
+}
+
+const judge = () => {
+  // ここで正誤判定をする
+}
+
 export default function QuizPage() {
-  const state = {
-    sentence: 'お腹周りの脂肪を落とすのに最も効果的なのは腹筋である',
-    answer: false
+  const [quizState, setQuiz] = useState(initialState)
+
+  useEffect(() => {
+    // ここでquizを取得するが今は固定値を返す
+    setQuiz(getQuiz('/quiz'))
+  }, [])
+
+  const pushMaru = () => {
+    setQuiz({
+      ...quizState,
+      answered: true,
+    })
   }
 
-  const menu = 'スクワット'
-  const answered = false
+  const pushBatu = () => {
+    setQuiz({
+      ...quizState,
+      answered: true,
+    })
+  }
 
   return (
     <Container>
       <Text align="center" fontSize="6xl" mt={5}>
-        Q {state.sentence}
+        Q. {quizState.sentence}
       </Text>
       <Flex mt={10}>
         <Box flex="1">
-          <Button colorScheme="red" width="100%" height={100}>
+          <Button
+            onClick={pushMaru}
+            colorScheme="red"
+            width="100%"
+            height={100}
+          >
             ○
           </Button>
         </Box>
         <Box flex="1">
-          <Button colorScheme="blue" width="100%" height={100}>
+          <Button
+            onClick={pushBatu}
+            colorScheme="blue"
+            width="100%"
+            height={100}
+          >
             ☓
           </Button>
         </Box>
       </Flex>
-      {
-        answered ? (
-          <Box>
-            <Center mt={10}>
-              <Text fontSize="4xl" align='center'>
-                正解
-              </Text>
-            </Center>
+      {quizState.answered ? (
+        <Box>
+          <Center mt={10}>
+            <Text fontSize="4xl" align="center">
+              正解
+            </Text>
+          </Center>
 
-            <Center mt={5}>
-              <Text fontSize="2xl" align='center'>
-                あなたは腹筋に関する知識がありますね
-                では他の筋肉を鍛えるために{menu}を10回やりましょう！！
-              </Text>
-            </Center>
+          <Center mt={5}>
+            <Text fontSize="2xl" align="center">
+              あなたは腹筋に関する知識がありますね では他の筋肉を鍛えるために
+              {quizState.menu}を10回やりましょう！！
+            </Text>
+          </Center>
 
-            <Center mt={5}>
-              <Link href='/'>
-                トップページへ
-              </Link>
-            </Center>
-          </Box>
-        ) : (
-          <p></p>
-        )
-      }
-          </Container>
+          <Center mt={5}>
+            <Link href="/">トップページへ</Link>
+          </Center>
+        </Box>
+      ) : (
+        <p></p>
+      )}
+    </Container>
   )
 }
