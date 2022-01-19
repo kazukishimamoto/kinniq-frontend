d1e01dfe81d691fe99ad5efc16b88380304be8b6
Kazuki Shimamoto <jakky0529@gmail.com>
2022-01-19 19:02:55 +0900

    かなりクソ実装だがクイズを3問にした

--- pages/quiz/index.tsx
+++ pages/quiz/index.tsx
@@ -2,106 +2,158 @@ import { Container, Flex, Box, Button, Text, Center } from '@chakra-ui/react'
 import { useEffect, useState } from 'react'
 import Link from 'next/link'
 
-const initialQuizState = {
-  sentence: '',
-  menu: '',
-  answer: null,
+interface Quiz {
+  id: number
+  sentence: string
+  menu: string
+  answer: boolean
 }
 
-const initialAnswerState = {
-  answered: false,
-  correction: null,
-  sentence: '',
-}
-
-const getQuiz = (path) => {
-  return {
-    sentence: 'お腹周りの脂肪を落とすのに最適な運動は腹筋である',
-    menu: '腹筋',
-    answer: false,
-    answered: false,
-  }
+interface Answer {
+  answered: boolean
+  correction: boolean | null
+  sentence: string
 }
 
 export default function QuizPage() {
-  const [quizState, setQuiz] = useState(initialQuizState)
-  const [answerState, setAnswer] = useState(initialAnswerState)
+  const [quizState, setQuiz] = useState<Quiz[]>([])
+  const [answerState, setAnswer] = useState<Answer[]>([])
 
   useEffect(() => {
     // ここでquizを取得するが今は固定値を返す
-    setQuiz(getQuiz('/quiz'))
+    setQuiz([
+      {
+        id: 1,
+        sentence: 'お腹周りの脂肪を落とすのに最適な運動は腹筋である',
+        menu: '腹筋',
+        answer: false,
+      },
+      {
+        id: 2,
+        sentence: 'お腹周りの脂肪を落とすのに最適な運動は腹筋である',
+        menu: '腹筋',
+        answer: false,
+      },
+      {
+        id: 3,
+        sentence: 'お腹周りの脂肪を落とすのに最適な運動は腹筋である',
+        menu: '腹筋',
+        answer: false,
+      },
+    ])
   }, [])
 
-  const pushAnswer = (answer) => {
-    if (answerState.answered) return
+  const pushAnswer = (answer, quiz) => {
+    if (answerState[quiz.id]?.answered) return
 
-    const correction = quizState.answer === answer
+    const correction = quiz.answer === answer
     const correctMessage = `すばらしい！
 あなたは既にある程度の筋トレ知識があるようですね！
-さあ、${quizState.menu}を５回やりましょう！！
+さあ、${quiz.menu}を５回やりましょう！！
   `
     const inCorrectMessage = `ざんねん！
 腹筋をしたからといってお腹周りの脂肪が落とせるわけではありません
 脂肪を落とすには大きな筋肉を使う運動、例えばスクワットなどが効果的です！
-さあ、間違ってしまったので${quizState.menu}を10回やりましょう！！`
-    setAnswer({
-      answered: true,
-      correction: correction,
-      sentence: correction ? correctMessage : inCorrectMessage,
-    })
+さあ、間違ってしまったので${quiz.menu}を10回やりましょう！！`
+
+    let tmp1 = answerState[0]
+    let tmp2 = answerState[1]
+    let tmp3 = answerState[2]
+    switch (quiz.id) {
+      case 1:
+        tmp1 = {
+          answered: true,
+          correction: correction,
+          sentence: correction ? correctMessage : inCorrectMessage,
+        }
+        break
+      case 2:
+        tmp2 = {
+          answered: true,
+          correction: correction,
+          sentence: correction ? correctMessage : inCorrectMessage,
+        }
+        break
+      case 3:
+        tmp3 = {
+          answered: true,
+          correction: correction,
+          sentence: correction ? correctMessage : inCorrectMessage,
+        }
+        break
+    }
+
+    setAnswer([tmp1, tmp2, tmp3])
   }
 
   return (
     <Container>
-      <Text align="center" fontSize="6xl" mt={5}>
-        Q. {quizState.sentence}
-      </Text>
-      <Flex mt={10}>
-        <Box flex="1" mr={5}>
-          <Button
-            onClick={() => pushAnswer(true)}
-            colorScheme="red"
-            width="100%"
-            height={150}
-            fontSize='6xl'
-          >
-            ○
-          </Button>
-        </Box>
-        <Box flex="1">
-          <Button
-            onClick={() => pushAnswer(false)}
-            colorScheme="blue"
-            width="100%"
-            height={150}
-            fontSize='6xl'
-          >
-            ✗
-          </Button>
-        </Box>
-      </Flex>
-      {
-        answerState.answered && (
-        <Box>
-          <Center mt={10}>
-            <Text fontSize="6xl" align="center" color={answerState.correction ? 'red' : 'blue'}>{`${
-              answerState.correction ? '正解' : '不正解'
-            }`}</Text>
-          </Center>
-          <Box borderRadius="lg" color='white' bg={ answerState.correction ? 'red.400' : 'cyan.400'} p={3} mt={6} mb={6} align="center">
-            <Text fontSize='2xl'>
-              {answerState.sentence}
+      {quizState.map((quiz, index) => {
+        return (
+          <Box key={index} pb={20}>
+            <Text align="center" fontSize="6xl" mt={5} key={quiz.sentence}>
+              Q{index + 1}. {quiz.sentence}
             </Text>
+            <Flex mt={10}>
+              <Box flex="1" mr={5}>
+                <Button
+                  onClick={() => pushAnswer(true, quiz)}
+                  colorScheme="red"
+                  width="100%"
+                  height={150}
+                  fontSize="6xl"
+                >
+                  ○
+                </Button>
+              </Box>
+              <Box flex="1">
+                <Button
+                  onClick={() => pushAnswer(false, quiz)}
+                  colorScheme="blue"
+                  width="100%"
+                  height={150}
+                  fontSize="6xl"
+                >
+                  ✗
+                </Button>
+              </Box>
+            </Flex>
+            {answerState[index]?.answered && (
+              <Box>
+                <Center mt={10}>
+                  <Text
+                    fontSize="6xl"
+                    align="center"
+                    color={answerState[index].correction ? 'red' : 'blue'}
+                  >{`${
+                    answerState[index].correction ? '正解' : '不正解'
+                  }`}</Text>
+                </Center>
+                <Box
+                  borderRadius="lg"
+                  color="white"
+                  bg={answerState[index].correction ? 'red.400' : 'cyan.400'}
+                  p={3}
+                  mt={6}
+                  mb={6}
+                  align="center"
+                >
+                  <Text fontSize="2xl">{answerState[index].sentence}</Text>
+                </Box>
+              </Box>
+            )}
           </Box>
-
-          <Center mt={5}>
-            <Button align="center" bg='gray.600' color='white' >
-              <Link href="/">トップページへ</Link>
-            </Button>
-          </Center>
-        </Box>
         )
-      }
+      })}
+      {answerState.every((state) => {
+        return state?.answered
+      }) && (
+        <Center mt={5}>
+          <Button align="center" bg="gray.600" color="white">
+            <Link href="/">トップページへ</Link>
+          </Button>
+        </Center>
+      )}
     </Container>
   )
 }
